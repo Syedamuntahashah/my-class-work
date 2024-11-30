7340ae46bcace96136440d4cc0689d8d9b016af3
syedamuntahashah <ualyualy7082@gmail.com>
2024-11-30 17:43:40 +0500

    This is my 11 class assignment

--- my-app/src/app/components/State.tsx
+++ my-app/src/app/components/State.tsx
@@ -7,18 +7,15 @@ const State = () => {
    const handleIncrement = ()=> {
       setCount(count + 1)
    }
-   const [minus , setMiuns] = useState(100)
-       const handleDecrement = () =>{
-        setMiuns(minus - 1)
-       }
+   const handleDecrement = () =>{
+    setCount(count - 1)
+    }
   
     return (
        <div className="flex flex-row justify-around">
          <h1 className="text-7xl">{count}</h1>
           <button className="mx-5 py-4 rounded-md bg-slate-400 p-4 m-4" onClick={handleIncrement}>
             Increment</button>
-        
-            <h1 className="text-7xl">{minus}</h1>
         <button className="mx-5 py-4 rounded-md bg-slate-400 p-4 m-4" onClick={handleDecrement}>
             Decrement</button>
         </div> 
